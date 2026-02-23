9ab328e6a558b5691f2546f079a2e9754abdfecc
ahhaakhil-afk <ahha.akhil@gmail.com>
2026-02-23 16:37:19 +0530

    Create SpecsOverlay component

---
+++ components/SpecsOverlay.tsx
@@ -0,0 +1,12 @@
+// SpecsOverlay.tsx
+import React from 'react';
+
+const SpecsOverlay: React.FC = () => {
+    return (
+        <div>
+            <h1>Specifications Overlay</h1>
+        </div>
+    );
+};
+
+export default SpecsOverlay;
