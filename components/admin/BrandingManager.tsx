6fe433881f84ba8bdf4e4f8ef9957421122a57eb
ahhaakhil-afk <ahha.akhil@gmail.com>
2026-02-23 16:39:48 +0530

    Create BrandingManager component

---
+++ components/admin/BrandingManager.tsx
@@ -0,0 +1,13 @@
+// BrandingManager.tsx
+import React from 'react';
+
+const BrandingManager: React.FC = () => {
+    return (
+        <div>
+            <h1>Branding Manager</h1>
+            <p>Manage your brand settings here.</p>
+        </div>
+    );
+};
+
+export default BrandingManager;
