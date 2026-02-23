beb42008e9d34a00f004f68143204f15e286ccb9
ahhaakhil-afk <ahha.akhil@gmail.com>
2026-02-23 16:36:11 +0530

    Create pages/index.tsx with main showroom component

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Showroom: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Showroom</h1>
+            {/* Add more showroom features here */}
+        </div>
+    );
+};
+
+export default Showroom;
