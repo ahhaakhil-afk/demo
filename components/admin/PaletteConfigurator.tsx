751bf184a1c9ae9a17274464064bb33a1859cc1e
ahhaakhil-afk <ahha.akhil@gmail.com>
2026-02-23 16:40:26 +0530

    Create PaletteConfigurator component

---
+++ components/admin/PaletteConfigurator.tsx
@@ -0,0 +1,14 @@
+// PaletteConfigurator.tsx
+
+import React from 'react';
+
+const PaletteConfigurator: React.FC = () => {
+    return (
+        <div>
+            <h1>Palette Configurator</h1>
+            {/* Add configuration UI here */}
+        </div>
+    );
+};
+
+export default PaletteConfigurator;
