1ae5261b72cc8316bad838df050af9c58deb120b
ahhaakhil-afk <ahha.akhil@gmail.com>
2026-02-23 16:36:58 +0530

    Create CategoryNav component

---
+++ components/CategoryNav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CategoryNav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Category 1</li>
+                <li>Category 2</li>
+                <li>Category 3</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default CategoryNav;
