234a8dd8273a6993149a55f4183db74754926354
ahhaakhil-afk <ahha.akhil@gmail.com>
2026-02-23 16:40:21 +0530

    Create MediaManager component

---
+++ components/admin/MediaManager.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MediaManager: React.FC = () => {
+    return (
+        <div>
+            <h1>Media Manager</h1>
+            {/* Add your media management components here */}
+        </div>
+    );
+};
+
+export default MediaManager;
