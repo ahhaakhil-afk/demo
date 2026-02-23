0c630ca075cdc5aa798049592621da56109eeb94
ahhaakhil-afk <ahha.akhil@gmail.com>
2026-02-23 16:40:15 +0530

    Create GlobalSettings component

---
+++ components/admin/GlobalSettings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GlobalSettings: React.FC = () => {
+  return (
+    <div>
+      <h1>Global Settings</h1>
+      {/* Add your global settings components here */}
+    </div>
+  );
+};
+
+export default GlobalSettings;
