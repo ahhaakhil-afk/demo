8865b2a136347adf7ede57b39d6d58425c46071d
ahhaakhil-afk <ahha.akhil@gmail.com>
2026-02-23 16:40:07 +0530

    Create AdminPrompt component

---
+++ components/AdminPrompt.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminPrompt: React.FC = () => {
+    return (
+        <div>
+            <h1>Admin Prompt Component</h1>
+            <p>This is a prompt for administrators.</p>
+        </div>
+    );
+};
+
+export default AdminPrompt;
