e670c5813904efc87923fd0b14191af91631d15a
ahhaakhil-afk <ahha.akhil@gmail.com>
2026-02-23 16:36:32 +0530

    Create MainShowroom component

---
+++ components/MainShowroom.tsx
@@ -0,0 +1,14 @@
+// MainShowroom.tsx
+
+import React from 'react';
+
+const MainShowroom: React.FC = () => {
+    return (
+        <div>
+            <h1>Main Showroom</h1>
+            {/* Add your showroom components here */}
+        </div>
+    );
+};
+
+export default MainShowroom;
